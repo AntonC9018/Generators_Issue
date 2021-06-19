2e7f575066c12aa41d24aa41154e28ca915a2fd4
AntonC9018 <anton.curm@gmail.com>
2021-06-19 23:14:07 +0300

    clean up

--- Generator/Generator.cs
+++ Generator/Generator.cs
@@ -1,16 +1,9 @@
 using Microsoft.CodeAnalysis;
-using System.Collections.Generic;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Text;
-using System.Linq;
-using System.Text;
-using System.Diagnostics;
 
-namespace MetaGenerator
+namespace Generator
 {
     [Generator]
-    public class Generator : ISourceGenerator
+    public class MyGenerator : ISourceGenerator
     {
         public void Initialize(GeneratorInitializationContext context)
         {
